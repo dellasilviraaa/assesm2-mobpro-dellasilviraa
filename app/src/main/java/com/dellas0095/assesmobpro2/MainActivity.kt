733650dbdad86aaf3c0a65c044af872a15d2fa0f
Della Silvira <dellasilvira24@gmail.com>
2025-05-08 13:38:10 +0700

    membuat tombol FAB

--- app/src/main/java/com/dellas0095/assesmobpro2/MainActivity.kt
+++ app/src/main/java/com/dellas0095/assesmobpro2/MainActivity.kt
@@ -1,40 +1,11 @@
 package com.dellas0095.assesmobpro2
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.data.EmptyGroup.data
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.dellas0095.assesmobpro2.model.Catatan
-import com.dellas0095.assesmobpro2.screen.MainViewModel
+import com.dellas0095.assesmobpro2.screen.MainScreen
 import com.dellas0095.assesmobpro2.ui.theme.Assesmobpro2Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,86 +14,6 @@ class MainActivity : ComponentActivity() {
             Assesmobpro2Theme {
                 MainScreen()
             }
-    }
-}
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(){
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
         }
-    ){ innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    val viewModel: MainViewModel = viewModel()
-    val data = emptyList<Catatan>()
-    val context = LocalContext.current
-
-    if (data.isEmpty()){
-        Column (
-            modifier = modifier.fillMaxSize().padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Text(text = stringResource(id = R.string.list_kosong))
-        }
-    }
-    else{
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
-    ) {
-        items(data) {
-            ListItem(catatan = it)
-            val pesan = context.getString(R.string.x_diklik, it.judul)
-            Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
-        }
-        HorizontalDivider()
-            }
-        }
-    }
-}
-
-@Composable
-fun ListItem(catatan: Catatan, onClick: () -> Unit) {
-    Column (
-        modifier = Modifier.fillMaxWidth().clickable { onClick() }. padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
-        Text(
-            text = catatan.judul,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = catatan.catatan,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
-        Text(text = catatan.tanggal)
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    Assesmobpro2Theme {
-        MainScreen()
-    }
     }
 }
--- app/src/main/java/com/dellas0095/assesmobpro2/screen/MainViewModel.kt
+++ app/src/main/java/com/dellas0095/assesmobpro2/screen/MainViewModel.kt
@@ -1,57 +1,58 @@
 package com.dellas0095.assesmobpro2.screen
 
+import androidx.lifecycle.ViewModel
 import com.dellas0095.assesmobpro2.model.Catatan
 
-class MainViewModel : viewModel(){
+class MainViewModel : ViewModel() {
 
     val data = listOf(
         Catatan(
-        1,
-        "Nama Pembeli: "
-        "tanggal order"
-        "jumlah order"
+            1,
+            "Nama Pembeli: ",
+            "tanggal order",
+            "jumlah order"
 
-    ),
+        ),
         Catatan(
             2,
-            "Nama Pembeli:"
-            "tanggal order"
+            "Nama Pembeli:",
+            "tanggal order",
             "jumlah order"
 
         ),
         Catatan(
             3,
-            "Nama Pembeli: "
-            "tanggal order"
+            "Nama Pembeli: ",
+            "tanggal order",
             "jumlah order"
         ),
         Catatan(
             4,
-            "Nama Pembeli: "
-            "tanggal order"
+            "Nama Pembeli: ",
+            "tanggal order",
             "jumlah order"
 
         ),
         Catatan(
             5,
-            "Nama Pembeli: "
-            "tanggal order"
+            "Nama Pembeli: ",
+            "tanggal order",
             "jumlah order"
 
         ),
         Catatan(
             6,
-            "Nama Pembeli: "
-            "tanggal order"
+            "Nama Pembeli: ",
+            "tanggal order",
             "jumlah order"
         ),
         Catatan(
             7,
-            "Nama Pembeli: "
-            "tanggal order"
+            "Nama Pembeli: ",
+            "tanggal order",
             "jumlah order"
 
-        )
+        ),
     )
 
 }
