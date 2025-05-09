cddb7fee3b8279499dbd5198b79179dabc7fcd5b
Della Silvira <dellasilvira24@gmail.com>
2025-05-09 13:08:12 +0700

    menyimpan data ke database

--- app/src/main/java/com/dellas0095/assesmobpro2/screen/DetailScreen.kt
+++ app/src/main/java/com/dellas0095/assesmobpro2/screen/DetailScreen.kt
@@ -1,6 +1,7 @@
 package com.dellas0095.assesmobpro2.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,7 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,24 +30,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.dellas0095.assesmobpro2.R
 import com.dellas0095.assesmobpro2.ui.theme.Assesmobpro2Theme
+import com.dellas0095.assesmobpro2.util.ViewModelFactory
 
-
-const val KEY_ID_CATATAN = "idCatatan"
+const val KEY_ID_BLUSHLY = "idBlushly"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(navController: NavController, id: Long? = null) {
-    val viewModel: MainViewModel = viewModel()
+fun DetailScreen(navController: NavHostController, id : Long? = null) {
+    val context = LocalContext.current
+    val factory = ViewModelFactory(context)
+    val viewModel: DetailViewModel = viewModel(factory = factory)
+
     var judul by remember { mutableStateOf("") }
     var catatan by remember { mutableStateOf("") }
 
@@ -53,6 +61,7 @@ fun DetailScreen(navController: NavController, id: Long? = null) {
         judul = data.judul
         catatan = data.catatan
     }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -73,21 +82,38 @@ fun DetailScreen(navController: NavController, id: Long? = null) {
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
+                    titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 actions = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = {
+                        if (judul == "" || catatan == ""){
+                            Toast.makeText(context, R.string.invalid, Toast.LENGTH_SHORT).show()
+                            return@IconButton
+                        }
+                        if (id == null) {
+                            viewModel.insert(judul, catatan)
+                        } else {
+                            viewModel.update(id, judul, catatan)
+                        }
+                        navController.popBackStack()
+                    }) {
                         Icon(
                             imageVector = Icons.Outlined.Check,
                             contentDescription = stringResource(R.string.simpan),
                             tint = MaterialTheme.colorScheme.primary
                         )
                     }
+                    if (id != null){
+                        DeleteAction {
+                            viewModel.delete(id)
+                            navController.popBackStack()
+                        }
+                    }
                 }
             )
         }
-
     ) { padding ->
+
         FormCatatan(
             title = judul,
             onTitleChange = { judul = it },
@@ -98,17 +124,50 @@ fun DetailScreen(navController: NavController, id: Long? = null) {
     }
 }
 
+@Composable
+fun DeleteAction(delete: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(R.string.lainnya),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(id = R.string.hapus))
+                },
+                onClick = {
+                    expanded = false
+                    delete()
+                }
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    Assesmobpro2Theme {
+        DetailScreen(rememberNavController())
+    }
+}
 
 @Composable
-fun FormCatatan(
-    title: String, onTitleChange: (String) -> Unit,
-    desc: String, onDescChange: (String) -> Unit,
-    modifier: Modifier
+fun FormCatatan(title: String, onTitleChange: (String) -> Unit,
+                desc: String, onDescChange: (String) -> Unit,
+                modifier: Modifier
 ) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         OutlinedTextField(
@@ -131,14 +190,5 @@ fun FormCatatan(
             ),
             modifier = Modifier.fillMaxSize()
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun DetailScreenPreview() {
-    Assesmobpro2Theme {
-        DetailScreen(rememberNavController())
     }
 }
---
+++ app/src/main/java/com/dellas0095/assesmobpro2/screen/DetailViewModel.kt
@@ -0,0 +1,50 @@
+package com.dellas0095.assesmobpro2.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dellas0095.assesmobpro2.model.Blushly
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class DetailViewModel(private val dao: Blushly) : ViewModel() {
+
+    private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+
+    fun insert(judul: String, isi: String) {
+        val catatan = Blushly(
+            tanggal = formatter.format(Date()),
+            judul = judul,
+            catatan = isi
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.insert(catatan)
+        }
+    }
+
+    suspend fun getCatatan(id: Long): Blushly? {
+        return dao.getBlushlyById(id)
+    }
+
+    fun update(id: Long, judul: String, isi: String) {
+        val catatan = Blushly(
+            id = id,
+            tanggal = formatter.format(Date()),
+            judul = judul,
+            catatan = isi
+        )
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.update(catatan)
+        }
+    }
+
+    fun delete(id: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.deleteById(id)
+
+        }
+    }
+}
