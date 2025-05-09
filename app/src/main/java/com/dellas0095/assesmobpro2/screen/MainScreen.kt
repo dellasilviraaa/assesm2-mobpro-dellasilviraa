46988159b221dd80c9541a79973ccb56c0303444
Della Silvira <dellasilvira24@gmail.com>
2025-05-09 14:11:20 +0700

    membuat togle menu list/ grid

--- app/src/main/java/com/dellas0095/assesmobpro2/screen/MainScreen.kt
+++ app/src/main/java/com/dellas0095/assesmobpro2/screen/MainScreen.kt
@@ -1,6 +1,7 @@
-package com.dellas0095.assesmobpro2.screen
+package com.dellas0095.assesmobpro2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,12 +11,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,6 +35,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,12 +48,17 @@ import com.dellas0095.assesmobpro2.R
 import com.dellas0095.assesmobpro2.model.Blushly
 import com.dellas0095.assesmobpro2.navigation.Screen
 import com.dellas0095.assesmobpro2.ui.theme.Assesmobpro2Theme
+import com.dellas0095.assesmobpro2.util.SettingsDataStrore
 import com.dellas0095.assesmobpro2.util.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavHostController) {
-
+    val dataStore = SettingsDataStrore(LocalContext.current)
+    val showList by dataStore.layoutFlow.collectAsState(true)
     Scaffold(
         topBar = {
             TopAppBar(
@@ -53,10 +67,30 @@ fun MainScreen(navController: NavHostController) {
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                actions = {
+                    IconButton(onClick = { CoroutineScope(Dispatchers.IO).launch {
+                        dataStore.saveLayout(!showList)
+                    }
+                    }) {
+                        Icon(
+                            painter = painterResource(
+                                if (showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_grid_view_24
+                            ),
+                            contentDescription = stringResource(
+                                if (showList) R.string.grid
+                                else R.string.list
+                            ),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         },
+
+
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
@@ -71,48 +105,71 @@ fun MainScreen(navController: NavHostController) {
             }
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding), navController)
+        ScreenContent(showList, Modifier.padding(innerPadding), navController)
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier, navController: NavHostController) {
+fun ScreenContent(showList: Boolean, modifier: Modifier = Modifier, navController: NavHostController) {
     val context = LocalContext.current
     val factory = ViewModelFactory(context)
     val viewModel: MainViewModel = viewModel(factory = factory)
     val data by viewModel.data.collectAsState()
 
     if (data.isEmpty()) {
-        Column (
-            modifier = modifier.fillMaxSize().padding(1.dp),
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(40.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Text(text = stringResource(id = R.string.list_kosong))
+        ) {
+            Text(
+                text = stringResource(R.string.list_kosong),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    } else {
+        if (showList) {
+            LazyColumn(
+                modifier = modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(bottom = 84.dp)
+            ) {
+                items(data) {
+                    ListItem(catatan = it) {
+                        navController.navigate(Screen.FormUbah.withId(it.id))
+                    }
+                    HorizontalDivider()
+                }
+            }
+        }
+        else{
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxSize(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 8.dp,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 84.dp)
+            ) {
+                items(data){
+                    GridItem(catatan = it) {
+                        navController.navigate(Screen.FormUbah.withId (it.id))
+                    }
+                }
+            }
         }
     }
-    else {
-        LazyColumn(
-            modifier = modifier.fillMaxSize(),
-            contentPadding = PaddingValues(bottom = 84.dp)
-        ){
-            items(data) {
-                ListItem(catatan = it) {
-                    navController.navigate(Screen.FormUbah.withId(it.id))
-                }
-                HorizontalDivider()
-            }
-        } }
 }
 
 @Composable
 fun ListItem(catatan: Blushly, onClick: () -> Unit) {
-    Column (
-        modifier = Modifier.fillMaxWidth()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
             .clickable { onClick() }
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
+            .padding(16.dp)
+    ) {
         Text(
             text = catatan.judul,
             maxLines = 1,
@@ -126,8 +183,36 @@ fun ListItem(catatan: Blushly, onClick: () -> Unit) {
         )
         Text(text = catatan.tanggal)
     }
-
 }
+@Composable
+fun GridItem(catatan: Blushly, onClick: () -> Unit){
+    Card(
+        modifier = Modifier.fillMaxSize().clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, DividerDefaults.color)
+    ){
+        Column (
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ){
+            Text(
+                text = catatan.judul,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = catatan.catatan,
+                maxLines = 4,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(text = catatan.tanggal)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
